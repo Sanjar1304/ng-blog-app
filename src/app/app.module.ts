947522e9ef6f5558609c029cb453d57c146b9d35
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-07-18 22:17:52 +0500

    Added terms-and-condition component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { FooterComponent } from './layouts/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SingleCategoryComponent } from './pages/single-category/single-category.component';
 import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { TermsAndConditionComponent } from './pages/terms-and-condition/terms-and-condition.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { SinglePostComponent } from './pages/single-post/single-post.component';
     FooterComponent,
     HomeComponent,
     SingleCategoryComponent,
-    SinglePostComponent
+    SinglePostComponent,
+    TermsAndConditionComponent
   ],
   imports: [
     BrowserModule,
