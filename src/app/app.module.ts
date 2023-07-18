acac7e05bd9b2c90ccfa19cca72c7dd298906cd3
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-07-18 22:43:49 +0500

    Added comment-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { TermsAndConditionComponent } from './pages/terms-and-condition/terms-an
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
 import { CommentFormComponent } from './comments/comment-form/comment-form.component';
+import { CommentListComponent } from './comments/comment-list/comment-list.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { CommentFormComponent } from './comments/comment-form/comment-form.compo
     TermsAndConditionComponent,
     ContactUsComponent,
     SubscriptionFormComponent,
-    CommentFormComponent
+    CommentFormComponent,
+    CommentListComponent
   ],
   imports: [
     BrowserModule,
